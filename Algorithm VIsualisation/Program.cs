c52522fbd3ccfd7a20268846da19508bce5cb0c8
ShrewdLuni <87246480+ShrewdLuni@users.noreply.github.com>
2024-07-11 18:31:52 +0300

    feat: add basic Program.cs structure

--- Algorithm VIsualisation/Program.cs
+++ Algorithm VIsualisation/Program.cs
@@ -1,6 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSignalR();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
 
 app.Run();
