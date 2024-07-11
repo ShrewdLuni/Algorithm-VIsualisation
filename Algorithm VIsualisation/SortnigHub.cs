589487f3d950d17bcd1d6ba03f31ac012de46b08
ShrewdLuni <87246480+ShrewdLuni@users.noreply.github.com>
2024-07-11 19:20:10 +0300

    feat: add SortArray method
    Add sort array method with insertion sort realizationto test SignalR hub

--- Algorithm VIsualisation/SortnigHub.cs
+++ Algorithm VIsualisation/SortnigHub.cs
@@ -11,4 +11,17 @@ public sealed class SortnigHub : Hub
     {
         await Clients.All.SendAsync("ReceiveMessage", $"{Context.ConnectionId}: {message}");
     }
+
+    public async Task SortArray(int[] arr)//Test InsertionSort
+    {
+        for (int i = 1; i < arr.Length; i++)
+        {
+            for (int j = i - 1; j >= 0 && arr[j + 1] < arr[j]; j--)
+            {
+                (arr[j + 1], arr[j]) = (arr[j], arr[j + 1]);
+                await Clients.All.SendAsync("SortStep", arr, j);
+            }
+        }
+        await Clients.All.SendAsync("SortComplete");
+    }
 }
