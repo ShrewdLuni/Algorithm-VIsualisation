2292a5cad28af30b9872a5ff8ae6d009755fb19b
ShrewdLuni <87246480+ShrewdLuni@users.noreply.github.com>
2024-07-12 23:21:13 +0300

    feat: add BubbleSort method

--- Algorithm VIsualisation/SortnigHub.cs
+++ Algorithm VIsualisation/SortnigHub.cs
@@ -12,7 +12,28 @@ public sealed class SortnigHub : Hub
         await Clients.All.SendAsync("ReceiveMessage", $"{Context.ConnectionId}: {message}");
     }
 
-    public async Task SortArray(int[] arr)//Test InsertionSort
+    public async Task BubbleSort(int[] arr)
+    {
+        bool swapped = false;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            swapped = false;
+            for (int j = 0; j < arr.Length - i; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    (arr[j + 1], arr[j]) = (arr[j], arr[j + 1]);
+                    await Clients.All.SendAsync("SortStep", arr, j);
+                    swapped = true;
+                }
+            }
+            if (swapped == false)
+                break;
+        }
+        await Clients.All.SendAsync("SortComplete");
+    }
+
+    public async Task InsertionSort(int[] arr)
     {
         for (int i = 1; i < arr.Length; i++)
         {
