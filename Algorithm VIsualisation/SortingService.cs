064e29f5a98fb0cfcb22de1a7b77bd87e1fc0ed8
ShrewdLuni <87246480+ShrewdLuni@users.noreply.github.com>
2024-07-20 13:26:51 +0300

    refactor: Created SortingService for more code readability

---
+++ Algorithm VIsualisation/SortingService.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Algorithm_VIsualisation
+{
+    public static class SortingService
+    {
+        public static async Task ShuffleAsync(int[] arr, int delay, IClientProxy clientProxy)
+        {
+            var random = new Random();
+            for (int i = arr.Length - 1; i > 0; i--)
+            {
+                int index = random.Next(i + 1);
+                (arr[index], arr[i]) = (arr[i], arr[index]);
+                await Task.Delay(delay);
+                await clientProxy.SendAsync("SortStep", arr, i);
+            }
+        }
+
+        public static async Task InsertionSortAsync(int[] arr, int delay, IClientProxy clientProxy)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                for (int j = i - 1; j >= 0 && arr[j + 1] < arr[j]; j--)
+                {
+                    (arr[j + 1], arr[j]) = (arr[j], arr[j + 1]);
+                    await Task.Delay(delay);
+                    await clientProxy.SendAsync("SortStep", arr, j);
+                }
+            }
+            await clientProxy.SendAsync("SortComplete");
+        }
+
+        public static async Task MergeSortAsync(int[] arr, int delay, IClientProxy clientProxy)
+        {
+            await MergeSortHelperAsync(arr, 0, arr.Length - 1, delay, clientProxy);
+            await clientProxy.SendAsync("SortComplete");
+        }
+
+        private static async Task MergeSortHelperAsync(int[] arr, int left, int right, int delay, IClientProxy clientProxy)
+        {
+            if (left < right)
+            {
+                int middle = (left + right) / 2;
+
+                await MergeSortHelperAsync(arr, left, middle, delay, clientProxy);
+                await MergeSortHelperAsync(arr, middle + 1, right, delay, clientProxy);
+
+                await MergeAsync(arr, left, middle, right, delay, clientProxy);
+            }
+        }
+
+        private static async Task MergeAsync(int[] arr, int left, int middle, int right, int delay, IClientProxy clientProxy)
+        {
+            int leftArrayLength = middle - left + 1;
+            int rightArrayLength = right - middle;
+
+            int[] leftArray = new int[leftArrayLength];
+            int[] rightArray = new int[rightArrayLength];
+
+            for (int i = 0; i < leftArrayLength; i++)
+                leftArray[i] = arr[left + i];
+            for (int j = 0; j < rightArrayLength; j++)
+                rightArray[j] = arr[middle + 1 + j];
+
+            int leftIndex = 0, rightIndex = 0;
+            int mergeIndex = left;
+
+            while (leftIndex < leftArrayLength && rightIndex < rightArrayLength)
+            {
+                if (leftArray[leftIndex] <= rightArray[rightIndex])
+                {
+                    arr[mergeIndex] = leftArray[leftIndex];
+                    leftIndex++;
+                }
+                else
+                {
+                    arr[mergeIndex] = rightArray[rightIndex];
+                    rightIndex++;
+                }
+                mergeIndex++;
+                Thread.Sleep(delay);
+                await clientProxy.SendAsync("SortStep", arr, mergeIndex);
+            }
+
+            while (leftIndex < leftArrayLength)
+            {
+                arr[mergeIndex] = leftArray[leftIndex];
+                leftIndex++;
+                mergeIndex++;
+                Thread.Sleep(delay);
+                await clientProxy.SendAsync("SortStep", arr, mergeIndex);
+            }
+
+            while (rightIndex < rightArrayLength)
+            {
+                arr[mergeIndex] = rightArray[rightIndex];
+                rightIndex++;
+                mergeIndex++;
+                Thread.Sleep(delay);
+                await clientProxy.SendAsync("SortStep", arr, mergeIndex);
+            }
+        }
+    }
+}
--- Algorithm VIsualisation/SortnigHub.cs
+++ Algorithm VIsualisation/SortnigHub.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Algorithm_VIsualisation;
+using Microsoft.AspNetCore.SignalR;
 
 public sealed class SortnigHub : Hub
 {
@@ -14,101 +15,16 @@ public sealed class SortnigHub : Hub
 
     public async Task Shuffle(int[] arr, int delay)
     {
-        var random = new Random();
-        for (int i = arr.Length - 1; i > 0; i--)
-        {
-            int index = random.Next(i + 1);
-            (arr[index], arr[i]) = (arr[i], arr[index]);
-            Thread.Sleep(delay);
-            await Clients.Caller.SendAsync("SortStep", arr, i, Context.ConnectionId);
-        }
+        await SortingService.ShuffleAsync(arr, delay, Clients.Caller);
     }
 
     public async Task InsertionSort(int[] arr, int delay)
     {
-        Console.WriteLine(Context.ConnectionId + " invoke InsertionSort ,delay is:" + delay);
-        for (int i = 1; i < arr.Length; i++)
-        {
-            for (int j = i - 1; j >= 0 && arr[j + 1] < arr[j]; j--)
-            {
-                (arr[j + 1], arr[j]) = (arr[j], arr[j + 1]);
-                Thread.Sleep(delay);
-                Console.WriteLine(delay);
-                await Clients.Caller.SendAsync("SortStep", arr, j, Context.ConnectionId);
-            }
-        }
-        await Clients.Caller.SendAsync("SortComplete");
+        await SortingService.InsertionSortAsync(arr, delay, Clients.Caller);
     }
 
     public async Task MergeSort(int[] arr, int delay)
     {
-        Console.WriteLine(Context.ConnectionId + " invoke MergeSort " + Clients.Caller);
-        await MergeSortHelper(arr, 0, arr.Length - 1,delay);
-        await Clients.Caller.SendAsync("SortComplete");
-    }
-
-    private async Task MergeSortHelper(int[] arr, int left, int right, int delay)
-    {
-        if (left < right)
-        {
-            int middle = (left + right) / 2;
-
-            await MergeSortHelper(arr, left, middle,delay);
-            await MergeSortHelper(arr, middle + 1, right,delay);
-
-            await Merge(arr, left, middle, right, delay);
-        }
-    }
-
-    private async Task Merge(int[] arr, int left, int middle, int right, int delay)
-    {
-        int leftArrayLength = middle - left + 1;
-        int rightArrayLength = right - middle;
-
-        int[] leftArray = new int[leftArrayLength];
-        int[] rightArray = new int[rightArrayLength];
-
-        for (int i = 0; i < leftArrayLength; i++)
-            leftArray[i] = arr[left + i];
-        for (int j = 0; j < rightArrayLength; j++)
-            rightArray[j] = arr[middle + 1 + j];
-
-        int leftIndex = 0, rightIndex = 0;
-        int mergeIndex = left;
-
-        while (leftIndex < leftArrayLength && rightIndex < rightArrayLength)
-        {
-            if (leftArray[leftIndex] <= rightArray[rightIndex])
-            {
-                arr[mergeIndex] = leftArray[leftIndex];
-                leftIndex++;
-            }
-            else
-            {
-                arr[mergeIndex] = rightArray[rightIndex];
-                rightIndex++;
-            }
-            mergeIndex++;
-            Thread.Sleep(delay);
-            await Clients.Caller.SendAsync("SortStep", arr, mergeIndex, Context.ConnectionId);
-        }
-
-        while (leftIndex < leftArrayLength)
-        {
-            arr[mergeIndex] = leftArray[leftIndex];
-            leftIndex++;
-            mergeIndex++;
-            Thread.Sleep(delay);
-            await Clients.Caller.SendAsync("SortStep", arr, mergeIndex, Context.ConnectionId);
-        }
-
-        while (rightIndex < rightArrayLength)
-        {
-            arr[mergeIndex] = rightArray[rightIndex];
-            rightIndex++;
-            mergeIndex++;
-            Thread.Sleep(delay);
-            await Clients.Caller.SendAsync("SortStep", arr, mergeIndex, Context.ConnectionId);
-        }
+        await SortingService.MergeSortAsync(arr, delay, Clients.Caller);
     }
 }
